05ffde9ab0c829fe280af13422aa00345f8a6506
Jordan <jordanveness8@gmail.com>
2025-07-20 16:06:48 +0100

    add name objects and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+type Data = {
+  id: number;
+  name: string;
+  year: number;
+  profession: string;
+};
+
+const names: string[] = ["Jordan", "John", "Jane", "Doe"];
+const data: Data[] = [
+  {
+    id: 1,
+    name: "Jordan",
+    year: 1996,
+    profession: "Doctor",
+  },
+  {
+    id: 2,
+    name: "John",
+    year: 1998,
+    profession: "Engineer",
+  },
+  {
+    id: 3,
+    name: "Jane",
+    year: 1991,
+    profession: "Teacher",
+  },
+  {
+    id: 4,
+    name: "Doe",
+    year: 1990,
+    profession: "Accountant",
+  },
+];
 
 function App() {
   const [isShown, setIsShown] = useState(false);
-  const [count, setCount] = useState(0)
+  const [showEmployee, setIsShownEmployee] = useState(false);
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -22,10 +58,28 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <button onClick={() => setIsShown(true)}>
-          name
-        </button>
-        {isShown && <p>Test</p>}
+        <button onClick={() => setIsShownEmployee(true)}>Show employees</button>
+        {showEmployee && (
+          <div>
+            {data.map((employee) => {
+              const { name, profession } = employee;
+              return (
+                <p key={employee.id}>
+                  {name}, {profession}
+                </p>
+              );
+            })}
+          </div>
+        )}
+        <button onClick={() => setIsShown(true)}>Show names</button>
+        {isShown && (
+          <div>
+            {names.map((name, index) => {
+              console.log(index);
+              return <p key={index}>{name}</p>;
+            })}
+          </div>
+        )}
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -34,7 +88,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
